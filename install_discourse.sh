58d87145720e47cce3e9d38540282d2b60509eac
IslamSaber <IsSaber@users.noreply.github.com>
2017-06-02 14:45:06 +0200

    Update install_discourse.sh

--- install_discourse.sh
+++ install_discourse.sh
@@ -10,11 +10,4 @@ mkdir /var/discourse
 git clone https://github.com/sparkysmart/discourse_docker.git /var/discourse/
 cd /var/discourse
 sudo chmod 777 -R /var/discourse/containers/
-
-#cp samples/standalone.yml containers/app.yml
-
-# scp /var/www/market/deploy/discourse/dev.yml ubuntu@beta.slickblox.com:/var/discourse/containers/app.yml
-# scp ~/eslam/work/market/deploy/discourse/dev.yml ubuntu@beta.slickblox.com:/var/discourse/containers/app.yml
-
-
-# ./launcher bootstrap app
+echo 'finsh preparing environment of discourse , grap app.yml file and start Bootstrab :)'
